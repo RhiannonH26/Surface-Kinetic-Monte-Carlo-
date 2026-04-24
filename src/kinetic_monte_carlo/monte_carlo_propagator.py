3e35c3183e7845626ce48fac485a843afc61e1cc
Rhiannon Hendley <rhiannonhendley1@gmail.com>
2026-04-24 12:29:50 -0400

    Made some changes. Untested. Planning to uproot data structure.

--- src/kinetic_monte_carlo/monte_carlo_propagator.py
+++ src/kinetic_monte_carlo/monte_carlo_propagator.py
@@ -1,5 +1,4 @@
 import numpy as np
-from typing import Callable
 
 def generate_rate_const_initial_list(N_grid: float,
                                     comps_properties: dict):
@@ -24,20 +23,21 @@ def generate_rate_const_initial_list(N_grid: float,
     ----------
     N_grid : int
         Number of surface locations along one dimension of the surface.
-    comps_properties : list(dict)
-        A list of dictionaries containing the properties of each element 
+    comps_properties : dict(dict)
+        A dictionary of dictionaries containing the properties of each element 
         in each dictionary. It must contain the following properties for each 
-        keyword: the element specified with "atom", "partial_pressure" in atm, 
+        dictionary (specified by the element): "partial_pressure" in atm, 
         "mass" in u (atomic units), and "k_ads" the rate constant in units of s^-1. 
         Note that the chosen name or "key" for each component in the first level 
         of the dictionary is unimportant. For example, one element of this list 
         could be:
     
     ```
-    {   "atom" : str, # atom identity / element
+    "<atom>" : {   # <atom> is the name of the element, str
         "partial_pressure" : float, # partial pressure of the component
         "mass" : float, # mass in u (g/mol) of the component
         "k_ads" : float, # adsorption rate constant
+        "k_des": float, # desorption rate constant
         ...
     }
     ```
@@ -52,13 +52,13 @@ def generate_rate_const_initial_list(N_grid: float,
     """
     rate_constant_properties = []
     rates = np.zeros((N_grid,N_grid)) # sum over all k
-    for k in range(len(comps_properties)):
+    for el in list(comps_properties.keys()):
         for l in range(N_grid):
             for w in range(N_grid):
-                myk = comps_properties[k].get("k_ads")
+                myk = comps_properties.get(el).get("k_ads")
                 mydict = {"i": None,
                           "j": [l,w],
-                          "i_atom": comps_properties[k].gets("atom")}
+                          "i_atom": el}
                 rate_constant_properties.append(mydict)
                 rates[l,w] = myk
     # note could have stored things a bit differently but this is how i chose to do it
@@ -89,11 +89,12 @@ def _choose_random_rate(rates: np.ndarray):
     cumsum = np.cumsum(rates)
     # start binary search, using numpy's search
     chosen = np.searchsorted(cumsum,rho_1*cumsum[-1]) 
-    return chosen
+    return chosen #### NEED TO ALSO REMOVE OTHER THINGS FOR SAME POSITION
 
 def propagate_monte_carlo_one_step(rate_constant_list: list,
                                    rates: np.ndarray,
                                    current_time: float,
+                                   comps_properties: dict,
                                    ):
     """Propagates the kinetic monte carlo algorithm in time by taking one step.
     
@@ -102,7 +103,8 @@ def propagate_monte_carlo_one_step(rate_constant_list: list,
     constant multiplied by a random number to obey the Poisson distribution (i.e. could
     leave faster than expected).
 
-    The rate constant chosen is removed from the list and a new one is added.
+    The rate constant chosen is removed from the list and a new one is added. Done according
+    to https://doi.org/10.3389/fchem.2019.00202.
     
     Parameters
     ----------
@@ -113,6 +115,14 @@ def propagate_monte_carlo_one_step(rate_constant_list: list,
         The numerical value of the rate constants.
     current_time : float
         The current time in the simulation in seconds.
+    comps_properties : list(dict)
+        A list of dictionaries containing the properties of each element 
+        in each dictionary. It must contain the following properties for each 
+        keyword: the element specified with "atom", "partial_pressure" in atm, 
+        "mass" in u (atomic units), and "k_ads" the rate constant in units of s^-1. 
+        Note that the chosen name or "key" for each component in the first level 
+        of the dictionary is unimportant. For example, one element of this list 
+        could be:
 
     Returns
     ----------
@@ -123,4 +133,33 @@ def propagate_monte_carlo_one_step(rate_constant_list: list,
         The numerical value of the rate constants with the new rate const.
     """
     # first randomly pick a number
+    rho_2 = np.random.random() # rho_1 is in the random rate chooser
+    # get k_tot
+    k_tot = np.sum(rates)
+    # draw a random process (index) using binary search
+    index = _choose_random_rate(rates)
+    # propagate according to the TOTAL rate constant to ensure that it occurs correctly
+    t -= np.log(rho_2)/k_tot
+    # update the rate lists by 
+    # (1) removing the current rate
+    rates = np.delete(rates, index) ### MIGHT BE INEFFICIENT, CONSIDER CHANGING DATA STRUCTURE
+    curr_rate_info = rate_constant_list.pop(index)
+    element = curr_rate_info.get('i_atom')
+    # (2) adding new rates for current position
+    # check to see what new rates are possible based on what rate was executed
+    if curr_rate_info.get('i') == None: 
+        # then we just executed an adsorption event
+        # the only next possible event is a desorption event, add that
+        kdes = comps_properties.get(element).get('k_des')
+        location = curr_rate_info.get("j") # should be of size (2,)
+        # add it to the things we care about
+        rates.append(kdes)
+        rate_constant_list.append({"i": None,
+                          "j": location,
+                          "i_atom": element})
+    elif curr_rate_info.get('j') == None:
+        # then we just executed a desorption event
+        # the next possiblity for the SITE is an adsorption event
+        # you could have adsorption of ANY of the molecules
+        pass
     return None
