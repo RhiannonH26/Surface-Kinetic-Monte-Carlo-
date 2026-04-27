5f97c1a78badcb6461943c4d427bde80c81b0201
Rhiannon Hendley <rhiannonhendley1@gmail.com>
2026-04-26 23:22:46 -0400

    Added surface coverage plots

--- src/kinetic_monte_carlo/make_plots.py
+++ src/kinetic_monte_carlo/make_plots.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+import matplotlib.patches as mpatches
 import numpy as np
 
 def make_grid(ax,N_grid,grid_vis,el_index,curr_time):
@@ -21,7 +22,7 @@ def make_grid(ax,N_grid,grid_vis,el_index,curr_time):
             if grid_vis[i,j] == 0:
                 circle = plt.Circle((i,j),0.45,color="grey",fill=False,linewidth=0.2)
             else:
-                circle = plt.Circle((i,j),0.45,color=el_index.get(int(grid_vis[i,j])),fill=True,linewidth=0.2)
+                circle = plt.Circle((i,j),0.45,color=el_index.get(int(grid_vis[i,j]))[0],fill=True,linewidth=0.2)
             ax.add_artist(circle)
     plt.xlim([-1.0, N_grid])
     plt.ylim([-1.0, N_grid])
@@ -29,6 +30,12 @@ def make_grid(ax,N_grid,grid_vis,el_index,curr_time):
     plt.scatter(np.arange(N_grid),np.arange(N_grid),s=0, facecolors='none')
     plt.axis('off')
     plt.title(f"t = {curr_time:.1f} s")
+    # make legend
+    patches = []
+    for elle in el_index:
+        patches.append(mpatches.Patch(color=el_index.get(elle)[0], label=el_index.get(elle)[1]))
+
+    ax.legend(handles=patches, loc='center left', bbox_to_anchor=(1, 0.5))
     #plt.show() ### SHOWS A PLOT
     return ax.artists 
 
@@ -54,13 +61,28 @@ def plot_trajectory(N_grid,grid_vis,el_index,t_uniform):
         repeat=False
     )
 
-    ani.save("trajectory.mp4", writer="ffmpeg", fps=20)
+    ani.save("trajectory.mp4", writer="ffmpeg", fps=5)
     plt.show()
-    
+
+def plot_surface_coverage(N_grid,grid_vis,el_index,times):
+    # first get the surface coverage for each element
+    nums = {l:[] for l in el_index}
+    l_max = np.max(list(el_index.keys()))
+    for i in range(len(grid_vis)):
+        for l in range(1,l_max+1):
+            nums[l].append(np.count_nonzero(grid_vis[i] == l)/N_grid**2) # this represents the surface covg
+    # now plot it
+    for l in range(1,l_max+1):
+        plt.plot(times,nums.get(l),color=el_index.get(l)[0],label=el_index.get(l)[1])
+    plt.xlabel('Time (s)')
+    plt.ylabel('Surface coverage, theta')
+    plt.legend()
+    plt.savefig("surface_coverage.png")
+    plt.show()
 
 if __name__ == "__main__":
     # get el_index from comps_properties
-    el_index = {1:"red",2:"blue"}
+    el_index = {1:["red","O"],2:["blue","C"]}
     grid_vis = np.random.randint(3,size=(4,50,50))
     t_uniform = np.linspace(0,10,4)
 
--- src/kinetic_monte_carlo/run_kmc.py
+++ src/kinetic_monte_carlo/run_kmc.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-from kinetic_monte_carlo.make_plots import plot_trajectory
+from kinetic_monte_carlo.make_plots import plot_trajectory, plot_surface_coverage
 from kinetic_monte_carlo.monte_carlo_propagator import propagate_monte_carlo_one_step,generate_rate_const_initial_list
 
 def generate_k_indices(comps_properties):
@@ -29,8 +29,6 @@ def map_to_time_const_grid(num_steps, max_time, grid_vis_over_time, time_grid):
 
     # clamp to valid range (prevent indices <0 or > -1)
     indices = np.clip(indices, 0, len(time_grid) - 1)
-    print(indices)
-    print(grid_vis_over_time[0])
 
     return equal_time_grid,np.array(grid_vis_over_time)[indices] # now projected onto a uniform grid
 
@@ -42,8 +40,7 @@ def run_full_length_monte_carlo(N_grid,comps_properties,max_time):
     k_indices = generate_k_indices(comps_properties)
 
     ### for storing
-    grid_vis_over_time = [grid_vis]
-    print(grid_vis_over_time)
+    grid_vis_over_time = [grid_vis.copy()]
     time_grid = [current_time]
     ####
 
@@ -58,23 +55,23 @@ def run_full_length_monte_carlo(N_grid,comps_properties,max_time):
 
 def main():
     comps_properties = {
-        "O" : {
+        "A" : {
             "partial_pressure" : 0.7,
             "mass" : 15.999,
-            "k_ads" : 5e-1,
+            "k_ads" : 7e-1,
             "k_des" : 2.0e-1,
         },
-        "C" : {
+        "B" : {
             "partial_pressure" : 0.3,
             "mass" : 12.01,
             "k_ads" : 1.0e-1,
-            "k_des" : 4.0e-2,
+            "k_des" : 1.0e-9,
         }
     }
-    max_time = 10 # seconds
-    N_grid = 20
-    num_steps = 10
-    el_index = {1: 'red', 2: 'blue'}
+    max_time = 170 # seconds
+    N_grid = 24
+    num_steps = 100
+    el_index = {1:["red","A"],2:["blue","B"]}
 
     print("Running monte carlo")
     grid_vis_over_time,time_grid = run_full_length_monte_carlo(N_grid,comps_properties,max_time)
@@ -82,7 +79,8 @@ def main():
     print("Done. Plotting now.")
     t_uniform,grid_vis_uniform = map_to_time_const_grid(num_steps, max_time, grid_vis_over_time, time_grid)
 
-    plot_trajectory(N_grid,grid_vis_uniform,el_index,t_uniform)
+    #plot_trajectory(N_grid,grid_vis_uniform,el_index,t_uniform)
+    plot_surface_coverage(N_grid,grid_vis_over_time,el_index,time_grid)
 
 if __name__ == "__main__":
     main()
